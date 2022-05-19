ba0b33a63d79aac0b5e1caa4ebc1e9d08c675388
Khaya <49770156+KDMpehle@users.noreply.github.com>
2022-05-19 13:57:29 +1000

    Add files via upload
    thermo functions library and skewT plot script added

---
+++ SkewT_ver2.py
@@ -0,0 +1,128 @@
+# -*- coding: utf-8 -*-
+"""
+Prototype functions for a skewT diagram in matplotlib
+
+@author: Khaya Mpehle
+"""
+
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+from Thermo_functions import *
+
+#from Thermo_functions import *
+#define some default inputs
+T0_DA = [n* 10 for n in range(-10,8)] #P0 temperatures for the dry adiabats
+T0_SA = [n*5 for n in range(-4,9)] #P0 temperatures of the saturated adiabats
+rs_init = [0.1, 0.4, 1, 2, 3, 5, 8, 12, 20] 
+P0 = 1050 # in hPa, the lowest pressure height
+Pf = 100 # in hPa, the highest pressure height 
+rotation = 35
+n = int(np.floor((P0-Pf)/50.))+1 # number of points in the dry adiabats
+#Define the transform for the skewT variable
+def skewT(T, P, rotation = rotation, P0 = P0): 
+    '''
+    A function to generate the X-axis variable of the skewT
+    plot.
+    -----
+    input:
+    T: The temperature. a 1-d array.
+    P: The pressure. a 1-d array. Shape must matche the temperature's
+    rotation: The rotation that skews the temperature variable. defaults
+    at 1050.
+    P0: The reference pressure taken as the Y = 0 on the diagram. Given as 
+    hPa. 
+    -----
+    output: 
+    X: the skewed temperature variable. 1-d array of equal shape to
+    the temperature and pressure arrays.
+    '''
+    return T + rotation * np.log(P0 / P)
+
+### This is just a test ###
+def blank_skewT(T0_DA = T0_DA, rs = rs_init,T0_SA = T0_SA, P0 = P0, Pf = Pf, n = n):
+    '''
+    Plot a barebones plot with dry adiabats
+    -----
+    input:
+    T0: an array of initial temperatures 
+    P0: lowest pressure level. Given in hPa
+    Pf: highest pressure level. Given in hPa
+    n: number of points in the dry adiabats.
+    -----
+    output:
+    An axis object showing the adiabats and isohumes
+    '''
+    #adiabats = dry_adiabats(T0, P0, Pf, n) # get the adiabats as an array
+    P = np.linspace(P0, Pf, n) # i will reverse and log these later. 
+    rs_gg  = np.array(rs) / 1000 # rs in g/g
+    fig, ax = plt.subplots(figsize = (10,10)) #initialise an axes object.
+    ax.set_yscale('log')
+    ax.invert_yaxis()
+    ax.set_yticks(P)
+    ax.get_yaxis().set_major_formatter(matplotlib.ticker.ScalarFormatter())
+    ax.yaxis.grid(True)
+    ax.margins(y = 0) # Zero margin between the diagram's xaxis and tick labels
+    ax.set_xlim(-35,45)
+    for index, label in enumerate(ax.yaxis.get_ticklabels()):
+        if index % 2 == 0: # this hides the pressure tick labels ending in 50 eg 1050, 950, 750 etcl   
+            label.set_visible(False)
+    for index, line in enumerate(ax.yaxis.get_major_ticks()):
+        if index % 2 == 0 or index == n-1: # this hides the tick line of labels ending in 50 and at the top of the plot.
+            line.tick1On = False # a way to remove the axis tick while retaining the grid line
+    #X = skewT(adiabats, P)
+    for k in range(len(T0_DA)): # loop over each adiabat, plot them
+        adiabat = (T0_DA[k] + 273.15) * (P / P[0])**0.286 -273.15 # convert to Kelvin for Poisson eqn then convert to celsius
+        X_adiabat = skewT(adiabat, P)
+        X_isotherm = skewT(T0_DA[k], P)
+        ax.plot(X_adiabat, P, color = 'g', alpha = 0.5 )
+        if abs(T0_DA[k]) < 1E-3:
+            ax.plot(X_isotherm, P, color = 'k', alpha = 0.5)
+        else:
+            ax.plot(X_isotherm, P, color = 'orange', alpha = 0.5)
+            ax.annotate('{}'.format(T0_DA[k]), (X_isotherm[-1], P[-1]), color = 'orange')
+    for k in range(len(rs)):
+        #P_trunc = P[ P >=400]
+        isohumes = isohume(P, rs_gg[k]) # get the temperature isohumes
+        X_isohume = skewT(isohumes, P)
+        ax.plot(X_isohume, P, '--', color = 'b', alpha = 0.5)
+        ax.annotate('{}'.format(rs[k]), (X_isohume[-int(n/2)],P[-int(n/2)]), color = 'b')
+    n_sat = int(np.floor((P0-Pf)/2.))+1 # number of points for the saturated adiabats
+    P_sat = np.linspace(P0, Pf, n_sat) # get the pressure intervals integrating saturated adiabat
+    for k in range(len(T0_SA)): # loop over and plot each saturated adiabat
+        saturated_adiabat = moist_adiabat(T0_SA[k], P_sat)
+        X_moist_adiabats = skewT(saturated_adiabat[0::25], P)
+        ax.plot(X_moist_adiabats, P, '--', color='g', alpha = 0.5)
+        ax.annotate('{}'.format(T0_SA[k]), (X_moist_adiabats[-2], P[-2]), color = 'g')
+    return ax
+
+#a sample trace to plot
+P = np.array([900, 850, 800, 700, 600, 500])
+T = np.array([15, 11.8, 9.2, 2.6, 2, -5.3])
+Td = np.array([8.5,3.8,7.2, -2.4, -36.5, -50.3])
+T_lifted,P_new = lift_trace(T,Td,P, 150)
+Tw = wetbulb_trace(T, Td, P) #get wet bulb trace
+print(P_new)
+t = skewT(T, P)
+td = skewT(Td, P)
+tw = skewT(Tw, P)
+t_lifted = skewT(T_lifted,P_new)
+ax = blank_skewT()
+ax.plot(t, P, color = 'r')
+ax.plot(td, P, color = 'r')
+ax.plot(tw, P, color = 'g')
+#ax.plot(t_lifted,P_new,'-o',color = 'magenta')
+plt.show()
+    
+ #20 degree saturated potential temperature adiabat test
+#n_sat = int(np.floor((P0-Pf)/2.))+1 # number of points for the saturated adiabats
+#P_sat = np.linspace(P0, Pf, n_sat) # get the standard pressure intervals
+#P = np.linspace(P0,Pf,n)
+#print(P_sat[0::50])
+#sat_adiabat = moist_adiabat(20, P_sat) # 20 degrees at 1000 hPa
+#print(len(sat_adiabat[0::50]))
+#X = skewT(sat_adiabat[0::50], P)
+#print(sat_adiabat[0::50],X)
+
+   
+    
---
+++ Thermo_functions.py
@@ -0,0 +1,286 @@
+# -*- coding: utf-8 -*-
+"""
+A script with functions useful in
+atmospheric thermodynamics
+
+@author: Khaya Mpehle
+"""
+import numpy as np
+import scipy.optimize
+# Define some parameters
+eps = 0.622 #g/g ratio of dry air specifc gas const to moist gas const.
+Lv = 2.5E6 #Latent heat of vaporisation
+Cp = 1003.5 #Specific heat capcity of dry air at constant pressure in Jkg-1K-1
+Rd = 287.058 # The specific gas constant of dry air in Jkg-1K-1 
+
+# Calculate the Saturation vapour pressure
+def vapour_pres_sat(T):
+    '''
+    A function for calculating the saturation vapour pressure
+    input: 
+    T: Temperature in degrees celcius. As a scalar or array of values
+    -----
+    output:
+    es: Saturation vapour pressure in hPa/mb. Returns an array or scalar 
+    depending on the temperature input.
+    
+    notes: The formuala used is August-Roche-Magnus formula for the saturation
+    vapour pressure. See "Improved Magnus' form approximation of Saturation
+    Vapour Pressure" released by NOAA.
+    '''
+    #Here you'd need to put in error checking. 
+    return 6.1094 * np.exp(17.625 * T / (T + 243.04))
+        
+def vapour_pres(Td):
+    '''
+    Function for calculating the vapour pressure given the dew point.
+    
+    input:
+    Td: dew point in degrees celcius. As a scalar or array
+    output:
+    e: The vapour pressure at the temperature associated with Td. Returns object
+    with the same shape as Td
+    '''
+    #Here put in the error checking.
+    return 6.1094 * np.exp(17.625 * Td / (Td + 243.04))
+def mix_ratio_sat(P, T):
+    '''
+    Function to calculate the saturation mixing at a particular pressure and
+    temperature.
+    -----
+    input:
+    T: Temperature in degrees celcius. As a scalar or array of values
+    P: Associated pressure. As a scalar or array. Must have the same shape
+    and data type as the temperature, with pressure and temp of the same level
+    at equivalent array index.
+    -----
+    output:
+    The saturation mixing ratio. Shape is the same as the shape of temp and 
+    pressure arrays.
+    '''
+    #Here you need to put in error checking
+    eps = 0.622 # g/g ratio of dry specific gas constant to mois gas const
+    return eps * vapour_pres_sat(T) / (P - vapour_pres_sat(T))
+
+def mix_ratio(P,Td):
+    '''
+    Function to calculate the saturation mixing at a particular pressure and
+    dew point.
+    -----
+    input:
+    Td: Dew point in degrees celcius. As a scalar or array of values
+    P: Associated pressure. As a scalar or array. Must have same shape and data
+    type as the temperature, with pressure and temp of the same level in 
+    corresponding array positions.
+    -----
+    output:
+    The mixing ratio in [WHAT UNITS?]
+    '''
+    eps = 0.622
+    return eps * vapour_pres(Td) / ( P - vapour_pres(Td))
+
+def isohume(P,r):
+    '''
+    Function to calculate the temperature-pressure profile when the mixing 
+    ratio rs is kept constant. 
+    -----
+    input:
+    P: The pressure in hPa. As a scalar or array of values
+    rs: The mixing ratio desired. Given in g/g (?)
+    -----
+    output:
+    Temperature in degreess celsius. Same shape as the inputted pressure. 
+    '''
+    eps = 0.622 
+    a = 6.1094 # constants for the August-Roche-Magnus formula
+    c = 17.625
+    d = 243.04
+    # The actual temperature function
+    arg = P * r / (r + eps) / 6.1094 
+    return 243.04 * np.log(arg) / (17.625 - np.log(arg))
+
+def dry_adiabats(T0, P0 = 1050., Pf = 100., n = 50) :
+    '''
+    Script to calculate the dry adiabats
+    -----
+    input:
+    T0: Initially temperature given as 1-d array
+    temp_type: Defaults to 'celcius' and converts temperatures to Kelvin.
+    P0: The surface pressure. positive scalar. Defaluts to 1050 hPa
+    Pf: The highest pressure chosen. Defaults 
+    -----
+    output: 
+    adiabats: an array with shape (n,len(T0)) holding the unsaturated 
+    adiabat curves.
+    '''
+    P = np.linspace(P0, Pf, n)
+    print(len(T0))
+    T= np.zeros((m, n)) # initialise array to hold the adiabats
+    for k in range(len(T0)):
+        T[k,:] = T0[k] * (P[:] / P[0])**3.496  #the 3.496 is Cp/R = 1003.5 / 287.06
+    return T 
+
+def SALR(P, T,temp_type = 'celsius'):
+    '''
+    Function to calculate the saturated adiabatic lapse rate (SALR) at a
+    particular pressure and temperature.
+    -----
+    input:
+    T: Temperature. Scalar or array
+    P: Pressure in hPa/mb. Scalar or array
+    temp_type: defaults to 'celsius' and converts temepratures to Kelvin. If 
+    not 'Kelvin' or 'celsius', returns an error.
+    -----
+    output: The saturated adiabatic lapse rate as C/hPa. same shape as input
+    temperature and pressure
+    '''
+    if temp_type == 'celsius':
+        r = mix_ratio_sat(P,T) # get the mixing ratio, with temp given in centigrade
+        T = T + 273.15 # convert temperature to Kelvin
+    elif temp_type == 'kelvin' or temp_type == 'Kelvin':
+        r = mix_ratio_sat(P, T - 273.15) # get the mixing ratio
+    else:
+        print('error: temp_type must be given as "celsius" or "Kelvin"')
+        return
+    numerator = Rd * T / Cp + r * Lv / Cp
+    denomenator = P * (1 + Lv**2 * r * eps / (Cp * Rd * T**2) )
+    #print(Rd / Cp, Lv/Cp, Lv**2 * eps/ (Cp * Rd))
+    return numerator / denomenator
+def Tlcl_eqn(T,Td,P):
+    '''
+    A function that sets up an equation relating Tl, the temperature at the LCL
+    for a parcel starting at initial point (T0,Td0,p0). The function can be
+    used in other scripts to fix T and Td, and then solve for Tl iteratively
+    -----
+    input:
+    T: temperature in celcius. Must be given as a scalar
+    Td: dew point in celcius. Must be given as scalar
+    P: Associated pressure. Must be given as a scalar
+    -----
+    output:
+    F: a function that should be equal to zero theoretically F(Tl) = 0, 
+    and should be solved iteratively to find the temperature at the LCL.
+    
+    Note: The formula is taken from 
+    "The computation of the equivalent potential temperature" (Bolton, 1980)
+    '''
+    e = vapour_pres(Td) # Get the vapor pressure
+    r = mix_ratio(P, Td) # YOU HAVE NOT YET DEFINED r(P, Td)
+    T = T + 273.15 #convert the temperature to kelvin
+    def F(Tl):
+        return (np.log(6.112 / e) + np.log(T / Tl) / (0.2854 * 1 - 2.8E-4 * r)
+                + 17.67 * (Tl - 273.15) / (Tl - 29.65))
+    return F, e, r, T 
+
+def find_Tlcl(T,Td,P):
+    '''
+    Implements the root finding procedure to find the lcl and associated 
+    pressure level
+    -----
+    input:
+    T: Temperature in celcius. Must be given as a scalar
+    Td: Dew point in celcius. Must be given as a scalar
+    P: Associated pressure in hPa/mb. Must be given as a scalar
+    -----
+    output:
+    [Tl,Pl]: The temperature and pressure at the lcl. two component array
+    '''
+    F, e, r, T = Tlcl_eqn(T,Td,P) # Get eqn, e, r and kelvin temp
+    Tl = 55 + 2840 / (3.5 * np.log(T) -np.log(e) -4.805) # initial guess
+    print(Tl)
+    #Tl = scipy.optimize.fsolve(F, Tl0)[0] # find the lcl temperature
+    #Now use Poisson's formula to find the pressure level
+    Pl = P * (Tl/T)**3.496  # the 3.496 is Cp/R = 1003.5 / 287.06
+    Tl = Tl - 273.15
+    return np.array([Tl,Pl])
+
+def moist_adiabat(Ti,P_vals):
+    '''
+    Solves for the moist saturated adiabat using the moist lapse rate
+    dT/dp = SALR(T, Td, P) starting from the level (Ti, Pi) using a 
+    simple forward Euler numerical method
+    Script assumes lapse rate is C/hPa
+    -----
+    input:
+    Ti: The initial temperature in degrees celcius. Given as scalar
+    P_vals: Array of pressure values over which to calculate the saturated adiabats.
+    n: The number of points on the saturated adiabat curve
+    -----
+    output:
+    saturated adibatic. a 2 x n array holding the temperature values in the 
+    first row (in degrees celcius) and the pressure values in the second row
+    (in hPa).
+    '''
+    #P_vals = np.linspace(Pi,Pf,n) # get the array of P_Vals
+    T_vals = np.zeros(len(P_vals))
+    deltaP = P_vals[1] - P_vals[0] # get the pressure increment.
+    T_vals[0] = Ti # populate first element of the temperature and pressure arrays
+    #P_vals[0] = Pi
+    for k in range(len(P_vals) - 1):
+        #print(SALR(P_vals[k],T_vals[k]))
+        T_vals[k+1] = T_vals[k] + SALR(P_vals[k],T_vals[k]) * deltaP
+    return T_vals
+
+
+def lift_trace(T, Td, P, DeltaP):
+    '''
+    To lift a temperature trace by some pressure increment deltaP.
+    The script will lift at the dry adiabatic lapse rate until the
+    lifting condensation level is reached (if it is), and then lift
+    along a saturated adiabat
+    -----
+    input:
+    T: Array of temperatures in sounding data. given as a one dimensional
+    array (1,n) [adapt for scalars?]
+    Td: Array of dew points in sounding data. 1D array (1,n).
+    P: Array of pressures in the sounding data. 1D array (1,n).
+    deltaP: The amount we wish to lift by. enter as hPa/mb.
+    enter as positive number(lift by 100 mb means DeltaP = 100)
+    -----
+    output: 
+    T_lifted: Array of the lifted the temperatures
+    P_lifted: Corresponding array of pressures.
+    '''
+    #list to hold initial conditions for the saturated lifting
+    P_new = P - DeltaP # get the new 
+    T_new = np.zeros(len(T))
+    for k in range(len(T)): 
+        Tlcl, Plcl = find_Tlcl(T[k],Td[k],P[k]) # get the lcl quantities
+        if P_new[k] >= Plcl:
+            T_new[k] = (T[k] + 273.15) * (P_new[k] / P[k])**0.286 - 273.15 #Dry adiabat lift to new level.
+        else: # if the parcel saturates before lifting is over, finish on Saturated adiabat
+            P_interval = np.linspace(Plcl, P_new[k],100) # figure out the interval size later
+            T_satcurve = moist_adiabat(Tlcl,P_interval)
+            T_new[k] = T_satcurve[-1] # only interested final temp after
+    return T_new, P_new # return the new temperatures and 
+
+def wetbulb_trace(T, Td, P, dP = 2.):
+    '''
+    A function to find the wet bulb temperature using a trace. I do not know
+    of a simple formula, iterative solver, etc for finding wet bulb
+    temperatures, so I simply lift to the LCL then descend down a saturated adiabat
+    back to the original pressure.
+    -----
+    T: Array of temperatures in sounding data. given as a one dimensional
+    array (1,n) [adapt for scalars?]
+    Td: Array of dew points in sounding data. 1D array (1,n).
+    P: Array of pressures in the sounding data. 1D array (1,n).
+    dP: The resolution of the saturated adiabats, defaults to
+    2 hPa
+    -----
+    output:
+    Tw: The wetbulb potential temperature trace. equal shape to the 
+    temperature
+    '''
+    Tw = np.zeros(len(T)) # initialise the wetbulb trace array.
+    print(Tw)
+    for k in range(len(T)):
+        Tlcl, Plcl = find_Tlcl(T[k], Td[k], P[k])
+        n_steps = int(np.floor((P[k]-Plcl) / dP)) + 1 # number of steps for saturated adiabats
+        print(n_steps)
+        P_vals = np.linspace(Plcl, P[k], n_steps) # array of pressure values over which to calculate the lcl
+        T_satcurve = moist_adiabat(Tlcl, P_vals)
+        Tw[k] = T_satcurve[-1] # The final temperature gives the wetbul temperature. 
+    return Tw
+
+            
